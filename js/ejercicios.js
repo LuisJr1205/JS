e835f8c313d373e47c42eed9699bc2c95340254c
Luis <lmadero@ari.group>
2023-03-13 19:15:17 -0600

    fin

--- js/ejercicios.js
+++ js/ejercicios.js
@@ -159,13 +159,9 @@ const numerosPrimos = (numero=undefined) =>{
 
 }
 
-numerosPrimos();
-numerosPrimos("1");
-numerosPrimos(-1);
 numerosPrimos(2);
-numerosPrimos(3);
-numerosPrimos(4);
 
+// Ejercicio 13
 const numeroPar = (numero= undefined) =>{
     if(numero===undefined) return console.error("No ingresaste el numero a evaluar");
 
@@ -184,13 +180,9 @@ const numeroPar = (numero= undefined) =>{
     }
 }
 
-numeroPar();
-numeroPar(0);
-numeroPar(-1);
-numeroPar(1);
 numeroPar(2);
 
-console.clear();
+// Ejercicio 14
 
 const conversionTemperaturas = (temperatura = undefined, unidades=undefined) =>{
     if(temperatura===undefined) return console.error("No ingresaste la temperatura");
@@ -270,6 +262,7 @@ const descuento = (precio=undefined, descuento=undefined) => {
 }
 descuento(100,10);
 
+// Ejercicio 17
 
 const aniosCuenta = (fecha=undefined) =>{
     if(fecha === undefined) return console.error("No ingreaste el precio");
@@ -278,6 +271,7 @@ const aniosCuenta = (fecha=undefined) =>{
 
     // Forma del video
     let hoyMenosFecha = new Date().getTime()-fecha.getTime(),
+        // Convierte la fecha a milisegundos
         aniosEnMS = 1000*60*60*24*365,
         aniosHumanos = Math.floor(hoyMenosFecha/aniosEnMS);
 
@@ -292,7 +286,6 @@ const aniosCuenta = (fecha=undefined) =>{
 }
 
 aniosCuenta(new Date(1984,4,23));
-aniosCuenta(new Date());
 aniosCuenta(new Date(2024,2,9));
 
 // Ejercicio 18
@@ -359,7 +352,7 @@ const validaCorreo = (correo = undefined)=>{
 
 validaCorreo("usuario@dominio.com");
 
-
+// Esta parte fue mas para tener una funcion que le eniviaramos la cdena y la exprecion con la que se va aevaluar.
 // Fusion de lasn funciones de correo y nombre ingresando la cadena y la expresion regular.
 
 const validaPatron = (cadena = "", patron=undefined)=>{
@@ -381,6 +374,372 @@ const validaPatron = (cadena = "", patron=undefined)=>{
 validaPatron("Luis Enrique",/[A-Za-zÑñÁÉÍÓÚáéíóú\s]+$/g);
 validaPatron("luismadero47@gmail.com",/[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,15})/i);
 validaPatron("luismadero47@gmail",new RegExp(/[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,15})/,'i'));
+
+
+// EJERCICIO 21
+
+const elevarCuadrado = (arreglo = undefined) =>{
+    if(arreglo === undefined || typeof arreglo === "string") return console.error("No ingresaste una arreglo");
+
+    if(!(arreglo instanceof Array)) return console.warn("No es un arreglo");
+
+    if (arreglo.length ===0 ) return console.warn("La cadena esta vacia");
+
+    for (let num  of arreglo) {
+        if(typeof num !== "number") return console.warn(`El valor ${num}, no es un numero`);
+    }
+
+    let nuevoArreglo=[];
+    for (let i = 0; i < arreglo.length; i++) {
+        nuevoArreglo[i] = Math.pow(arreglo[i],2);    
+    }
+    
+    return console.info(`El primer arreglo es:${arreglo}, arreglo elevado al cuadrado${nuevoArreglo}`);
+
+    //otra formad de colocarlo es con el .map
+    // const nuevoArreglo = arreglo.map(el => el*el);
+    // return console.info(`El primer arreglo es:${arreglo}, arreglo elevado al cuadrado${nuevoArreglo}`);
+
+}
+
+elevarCuadrado([1,4,5]);
+
+//EJERCICIO 22
+const numeroAltoBajo = (arreglo=undefined) =>{
+    if(arreglo === undefined || typeof arreglo === "string") return console.error("No ingresaste una arreglo");
+
+    if(!(arreglo instanceof Array)) return console.warn("No es un arreglo");
+
+    if (arreglo.length ===0 ) return console.warn("La cadena esta vacia");
+
+    for (let num  of arreglo) {
+        if(typeof num !== "number") return console.warn(`El valor ${num}, no es un numero`);
+    }
+
+    return console.info(`El numero mas alto es:${Math.max(...arreglo)} el numero mas bajo es:${Math.min(...arreglo)} de la cadena ${arreglo}`);
+}
+// numeroAltoBajo("hjagda");
+numeroAltoBajo([1,4,5,99,-60]);
+
+// EJERCICIO 23
+const arregloPares = (arreglo=undefined) =>{
+    if(arreglo === undefined || typeof arreglo === "string") return console.error("No ingresaste una arreglo");
+
+    if(!arreglo instanceof Array) return console.warn("No es un arreglo");
+
+    if (arreglo.length ===0 ) return console.warn("La cadena esta vacia");
+    
+    for (let num  of arreglo) {
+        if(typeof num !== "number") return console.warn(`El valor ${num}, no es un numero`);
+    }
+
+
+    //  let    residuo=0,
+    //         p=0,ip=0,
+    //         arregloPar=[],
+    //         arregloImpar=[];
+    // for (let i = 0; i < arreglo.length; i++) {
+    //     residuo = arreglo[i] % 2;
+    //     if (residuo === 0) {
+    //         arregloPar[p]= arreglo[i];
+    //         p++;
+    //     }else{
+    //         arregloImpar[ip]= arreglo[i];
+    //         ip ++;
+    //     }
+ 
+    // }
+
+    // return console.info(`Par:${arregloPar} impar: ${arregloImpar}`);
+
+
+    // Otra forma de hacerlo con los nuevos metodos
+    return console.info({
+        pares: arreglo.filter(num => num % 2 ===0),
+        impares: arreglo.filter(num => num % 2 ===1)
+    })
+}
+
+arregloPares([1,2,3,4,5,6,7,8,9,0]);
+
+console.clear();
+
+// EJERCICIO 24
+
+const arregloOrdernado = (arreglo=undefined) =>{
+    if(arreglo === undefined || typeof arreglo === "string") return console.error("No ingresaste una arreglo");
+
+    if(!arreglo instanceof Array) return console.warn("No es un arreglo");
+
+    if (arreglo.length ===0 ) return console.warn("La cadena esta vacia");
+    
+    for (let num  of arreglo) {
+        if(typeof num !== "number") return console.warn(`El valor ${num}, no es un numero`);
+    }
+
+    return console.info({
+        // arreglo, 
+        ascendente : arreglo.map(el => el).sort(),
+        descendente : arreglo.map(el => el).sort().reverse()
+    });
+}
+
+arregloOrdernado([7,5,7,8,6]);
+
+// EJERCICIO 25
+
+const arregloDuplicados = (arreglo = undefined) =>{
+    if(arreglo === undefined || typeof arreglo === "string") return console.error("No ingresaste una arreglo");
+
+    if(!arreglo instanceof Array) return console.warn("No es un arreglo");
+
+    if (arreglo.length ===0 ) return console.warn("Arreglo vacio");
+
+    if (arreglo.length ===1 ) return console.warn("El arreglo debe tener al menos 2 elementos");
+
+    let nuevo = arreglo.filter((element,i)=>{
+        return arreglo.indexOf(element) === i;
+      })
+
+    return console.info(`El arreglo sin duplicados es ${nuevo}`);    
+
+    // Otras 2 formas de resolverlo, evalua por elemento como un for anidado
+    /*return console.info({
+        original: arreglo,
+        sinDuplicados: arreglo.filter((value,index,self)=>self.indexOf(value)===index)
+    })*/
+
+    // EL set me permite formar un arreglo sin duplicaciones
+
+    /*return console.info({
+        original: arreglo,
+        sinDuplicados: [... new Set(arreglo)]
+    })*/
+
+
+}
+
+arregloDuplicados(["x", 10, "x", 2, "10", 10, true, true]);
+
+// EJERCICIO 26
+
+const promedioArreglo = (arreglo=undefined) =>{
+    if(arreglo === undefined || typeof arreglo === "string") return console.error("No ingresaste una arreglo");
+
+    if(!arreglo instanceof Array) return console.warn("No es un arreglo");
+
+    if (arreglo.length ===0 ) return console.warn("La cadena esta vacia");
+
+    for (let num  of arreglo) {
+        if(typeof num !== "number") return console.warn(`El valor ${num}, no es un numero`);
+    }
+
+    // let promedio = arreglo.reduce((acumulado,actual)=>acumulado+actual)/arreglo.length;
+
+    // return console.info(`El promedio del arreglo es ${promedio}`)
+
+    // Otra forma de hacerlo
+
+    return console.info(
+        arreglo.reduce((total,num, index, arreglo)=>{
+            total +=num;
+            if(index === arreglo.length-1){
+                return `El promedio de ${arreglo.join(" + ")} es ${total/arreglo.length}`;
+            }else{
+                return total;
+            }
+        })
+    )
+
+
+}
+
+promedioArreglo([9,8,7,6,5,4,3,2,1,0]);
+console.clear();
+
+// Ejercicio 27 creacion de una clase
+class Pelicula{
+    constructor({id,titulo,director,anio,paises,generos,calificacion}){
+        this.id=id;
+        this.titulo=titulo;
+        this.director=director;
+        this.anio=anio;
+        this.paises=paises;
+        this.generos=generos;
+        this.calificacion=calificacion;
+
+        this.validarId(id);
+        this.validarTitulo(titulo);
+        this.validarDirector(director);
+        this.validarAnio(anio);
+        this.validarPais(paises);
+        this.validarGeneros(generos);
+        this.validarCalificacion(calificacion);
+    }
+
+//atributo estatico 
+static get listaGeneros(){
+    return ["Action", "Adult", "Adventure", "Animation", "Biography", "Comedy", "Crime", "Documentary" ,"Drama", "Family", "Fantasy", "Film Noir", "Game-Show", "History", "Horror", "Musical", "Music", "Mystery", "News","Reality-TV", "Romance", "Sci-Fi", "Short", "Sport", "Talk-Show", "Thriller", "War", "Western"];
+}
+// Elemento estatico
+static generosAceptados(){
+    return console.info(`Los generos aceptados son: ${Pelicula.listaGeneros.join(", ")}`);
+}
+
+    
+validarCadena (propiedad, valor){
+        if(!valor) return console.warn(`${propiedad} esta vacio`);
+
+        if(typeof valor !== "string") return console.error(`${propiedad} "${valor}" no es una cadena de texto`);
+    
+        return true;
+}  
+
+validarNumeros (dato,valor){
+        if(!valor) return console.warn(`${dato} esta vacio`);
+
+        if(typeof valor !== "number") return console.error(`${dato} "${valor}" no es un numero`);
+    
+        return true;
+}
+
+validarArreglos (nom,arreglo){
+    if(!arreglo) return console.warn(`${nom} esta vacio`);
+
+    if(!(arreglo instanceof Array)) return console.warn(`${nom}No es un arreglo`);
+
+    if(arreglo.length === 0) return console.error(`El arreglo ${nom} esta vacio`);
+
+    for (let num  of arreglo) {
+        if(typeof num !== "string") return console.warn(`El valor ${num}, del arreglo no es un ${nom}`);
+    }
+
+    return true;
+}
+
+
+
+validarId (id) {
+    if(this.validarCadena("ID",id))
+        if (!(/^([a-z]){2}([0-9]){7}$/i.test(id))) return console.error(`"ID "${id}" no es valido, dee tener 9 caracteres 2 letras minusculas y 7 numeros`);    
+}
+
+validarTitulo (titulo){
+    if(this.validarCadena("Titulo",titulo))
+    if (titulo.length > 100 ) return console.error("El titulo no debe de exceder mas de 100 cacteres");
+    
+}
+
+validarDirector(director){
+    if(this.validarCadena("Director",director))
+    if (director.length > 50 ) return console.error("El Director no debe de exceder mas de 50 cacteres");
+}
+
+validarAnio(anio){
+    if(this.validarNumeros("Año",anio))
+    if (!(/^([0-9]){4}$/.test(anio))) return console.error(`Año "${anio}" no es valido, debe ser un numero de 4 digitos`);    
+}
+
+validarPais (paises){
+    if(this.validarArreglos("Paises",paises));
+}
+
+validarGeneros (generos){
+    if(this.validarArreglos("Generos",generos)){
+        for (let genero of generos) {
+            // console.log(genero, Pelicula.listaGeneros.includes(genero));
+            if(!Pelicula.listaGeneros.includes(genero)){
+                console.error(`Genero incorrectos"${generos.join(", ")}"`);
+                Pelicula.generosAceptados();
+
+            }
+        }
+    }
+
+}
+
+validarCalificacion (calificacion){
+    if(this.validarNumeros("Calificacion",calificacion));
+    
+    if (calificacion <0 || calificacion >10 ){
+        return console.error(`El valor de la calificaion debe de ser entre 0 y 10`);
+    }else{
+        this.calificacion = calificacion.toFixed(1);
+    }
+    /*if(this.validarNumeros("Calificacion",calificacion))
+        return (calificacion<0 || calificaion>10)
+        ? console.error(`La calificacion tiene que estar en un rango entre 0 y 10`)
+        : this.calificacion =calificacion.toFixed(1)*/
+
+}
+
+verDatos (){
+    console.info(`ID: ${this.id}
+    Titulo: ${this.titulo}
+    Director: ${this.director}
+    Año: ${this.anio}
+    Paises: ${this.paises.join("-")}
+    Generos: ${this.generos.join(", ")}
+    Calificacion ${this.calificacion}`)
+}
+
+}
+
+// Pelicula.generosAceptados();
+// const peli = new Pelicula({
+//     id: "AQ1234567",
+//     titulo: "Ant Man:Quantunmania",
+//     director: "Kevin Fage",
+//     anio: 2009,
+//     paises: ["Estados Unidos"],
+//     generos: ["Action","Adventure","Fantasy"],
+//     calificacion: 7.25,
+// });
+
+// peli.verDatos();
+
+// console.log(peli);
+
+const pruebaPeliculas=[
+    {
+        id:"AE7654321",
+        titulo: "Avengers: End Game",
+        director: "Anthony y Joe Russo",
+        anio: 2019,
+        paises: ["Estados Unidos"],
+        generos: ["Action","Adventure","Drama"],
+        calificacion: 8.4
+    },
+    {
+        id:"CE3215648",
+        titulo: "CREED III",
+        director: "Michael B. Jordan",
+        anio: 2023,
+        paises: ["Estados Unidos"],
+        generos: ["Sport","Drama"],
+        calificacion: 7.3
+    },
+    {
+        id:"HU7654521",
+        titulo: "Hasta el ultimo hombre",
+        director: "Mel Gibson",
+        anio: 2019,
+        paises: ["Estados Unidos","Australia"],
+        generos: ["Biography","History","Drama"],
+        calificacion: 8.1
+    }
+];
+
+pruebaPeliculas.forEach(element => new Pelicula(element).verDatos());
+
+
+
+
+
+
+
+
+
 
 
 
@@ -431,4 +790,3 @@ repetirTexto("Hola Mundo",3);
 repetirTexto("Hola mundo",0);
 repetirTexto("Hola mundo",-10);
 repetirTexto("",10);*/
-
